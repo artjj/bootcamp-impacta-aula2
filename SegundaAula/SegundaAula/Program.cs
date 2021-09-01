ed9d4f43bb62758255ddaccf5efd33a89a54c7cf
artjj <62916393+artjj@users.noreply.github.com>
2021-09-01 16:50:41 -0300

    Aula 2 - Estruturas condicionais e operadores

---
+++ SegundaAula/SegundaAula/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SegundaAula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 16;
+            bool isAdulto = true;
+            decimal valor = 10.55m;
+            double valor1 = 10.55;
+            char sigla = 'C';
+            string estado = "São Paulo";
+
+            var teste = "Teste";
+
+            if (idade >= 18)
+            {
+                teste = "Maior de idade";
+                isAdulto = true;
+            }
+            else if(estado == "São Paulo")
+            {
+                teste = "Paulista";
+            }
+
+            /*
+            switch (idade)
+            {
+                case <= 2: 
+                    teste = "Neném";
+                    break;
+                case > 2 && < 18:
+                    teste = "Criança";
+                    break;
+                case >= 18 && <= 65:
+                    teste = "Adulto";
+                    break;
+                case > 65:
+                    teste = "Senior";
+                    break;
+            }
+            */
+            Console.WriteLine(teste);
+        }
+    }
+}
